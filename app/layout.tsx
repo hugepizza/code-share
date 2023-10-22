a2aa9f0a32f2432dac444ad7a0ccae08237e1649
hugepizza <wangleilei950325@gmail.com>
2023-10-22 23:29:29 +0800

    fix mobile input zoomout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import SessionProvider from "./providers/nextauth";
 export const metadata: Metadata = {
   title: "Share Code",
   description: "Share your application code",
+  viewport:
+    "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
 };
 
 export default function RootLayout({
